730fe293463eb3eabedb1fd66dbf5c1e8aef42dc
Vyapti-Shah <164487749+Vyapti-Shah@users.noreply.github.com>
2024-08-03 01:40:49 +0530

    Add files via upload

---
+++ hideandshowhtml.js
@@ -0,0 +1,14 @@
+// HIDE AND SHOW HTML
+
+const mybutton = document.getElementById("mybutton");
+const myImg = document.getElementById("myImg");
+
+mybutton.addEventListener("click", event => {
+    if(myImg.style.visibility === "hidden"){
+        myImg.style.visibility = "visible";
+        mybutton.textContent = "Hide";
+    } else {
+        myImg.style.visibility = "hidden";
+        mybutton.textContent = "Show";
+    }
+}); 
